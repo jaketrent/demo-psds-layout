299b705e724b25f8f7a81ff2d9f7d4b34341d5ea
jaketrent <trent.jake@gmail.com>
2019-03-19 11:31:36 -0600

    start eq col layout

---
+++ src/card.js
@@ -0,0 +1,20 @@
+import Card from '@pluralsight/ps-design-system-card/react'
+import React from 'react'
+import styleable from 'react-styleable'
+
+import css from './card.module.css'
+
+export default styleable(css)(function Card(props) {
+  return (
+    <div className={props.css.card}>
+      <Card
+        size={Card.sizes.medium}
+        title={<Card.Title>Super Course!</Card.Title>}
+        metadata1={['Jim Cooper']}
+        image={
+          <Card.Image src="https://design-system.pluralsight.com/static/img/course1.jpg" />
+        }
+      />
+    </div>
+  )
+})
--- src/equal-column.js
+++ src/equal-column.js
@@ -1,5 +1,16 @@
+import { EqualColumnLayout } from '@pluralsight/ps-design-system-layout/react'
 import React from 'react'
 
-export default function EqualColumn() {
-  return 'equal column'
+import Card from './card.js'
+
+export default function EqualColumn({ count = 8 }) {
+  return (
+    <EqualColumnLayout count={EqualColumnLayout.counts.four}>
+      {Array(count)
+        .fill(null)
+        .map(_ => (
+          <Card />
+        ))}
+    </EqualColumnLayout>
+  )
 }
